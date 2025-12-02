0971321b6ade476fe8b2159d27ed631c977a413a
Gerard de Brieder <smeevil@me.com>
2025-12-02 23:25:48 +0100

    support for moving lines

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -25,3 +25,11 @@ map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show Diagnostic" })
 map("n", "gl", vim.diagnostic.open_float, { desc = "Show Diagnostic" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Move lines up/down (like IntelliJ's Option+Shift+Up/Down)
+map("n", "<M-S-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<M-S-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+map("i", "<M-S-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
+map("i", "<M-S-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
+map("v", "<M-S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<M-S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
